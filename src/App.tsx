0f7f8d22fb35c276cc4ea698ca813ef44c735dd4
jbeat <jbeat0630@gmail.com>
2025-01-21 11:29:55 +0900

    chore: createBrowserRouter 테스트 등

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/home'
 import Blog from './pages/blog'
 import NotFound from './pages/NotFound.tsx'
 import Post from './pages/post'
 import PostDetail from './pages/post/Detail'
 
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/blog',
+    element: <Blog />,
+  },
+  {
+    path: '/post',
+    element: <Post />,
+  },
+  {
+    path: '/post/:id',
+    element: <PostDetail />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  }
+]
+
+const router = createBrowserRouter(routes);
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   )
 }
 
